08834ae260f5e8795e923c8af54fb13b0499c43e
Prathamesh <prathameshranmale3647@gmail.com>
2026-05-16 23:16:34 +0530

    post login page

--- routes/user.js
+++ routes/user.js
@@ -3,44 +3,59 @@ const wrapAsync = require("../utils/wrapAsync");
 const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
 });
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try{  
-        let {username,email,password} = req.body;
-        //Creates new Mongoose user object.
-        const newUser = new User({username,email});//Password is NOT added manually because Passport handles it securely.
-        //save in db
-        const registeredUser = await User.register(newUser,password);
-        console.log(registeredUser);
-        req.flash("success","Welcome to wanderlust");
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      //Creates new Mongoose user object.
+      const newUser = new User({ username, email }); //Password is NOT added manually because Passport handles it securely.
+      //save in db
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", "Welcome to wanderlust");
         res.redirect("/listings");
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
     }
-    catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");
-    }
-}));
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
+  }),
+);
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
 });
 // This is for authenticate user if didn't go to login and show flash error
-router.post("/login",passport.authenticate('local',{failureRedirect:"/login",failureFlash:true}),
-    async(req,res)=>{
-        req.flash("success","Welcome back !!!");
-        res.redirect("/listings");
-    }
+router.post(
+  "/login",saveRedirectUrl,
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back !!!");
+    let redirectUrl = res.locals.redirectUrl || "listings";
+    // console.log(redirectUrl)
+    res.redirect(redirectUrl);
+  },
 );
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","You successfully logged out !!!");
-        res.redirect("/listings");
-    })
-})
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You successfully logged out !!!");
+    res.redirect("/listings");
+  });
+});
 
 module.exports = router;
